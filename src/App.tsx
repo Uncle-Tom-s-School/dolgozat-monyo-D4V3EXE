15fe8bd01181c4e4b4ef6ae8c0da589e2a6419ab
oxy <davidtoth.video@gmail.com>
2025-10-21 12:57:55 +0200

    freaky

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import BeerCard from "./components/BeerCard"
+import { useEffect, useState } from "react"
+import BEERCARD, {type BeerCardType} from "./components/BeerCard"
+
+
 
 const App = () => {
+  const [beerData, setBeerData] = useState<BeerCardType[]>([])
+
+  useEffect(() => {
+    fetch("data.json")
+      .then(response => response.json())
+      .then(data => setBeerData(data))
+  }, [])
+  
   return (
     <div className="beerCardWrapper">
-      <BeerCard />
+      {beerData.map(beer => <BEERCARD {...beer}/>)}
     </div>
   )
 }
--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -1,6 +1,22 @@
-const BeerCard = () => {
+export type BeerCardType = {
+  image: string
+  name: string
+  abv: string
+  volume: string
+  price: number
+  available: boolean
+}
+
+const BeerCard = (props: BeerCardType) => {
   return (
-    <div className='beerCard'>Beer Card</div>
+    <div className='beerCard'>
+    <img src={props.image} alt="" />
+    <h1>{props.name}</h1>
+    <p>ABV: {props.abv}%</p>
+    <p>Volume: {props.volume}ml</p>
+    <p>Price: {props.price} Ft</p>
+    <p>{props.available ? "Raktáron" : "Nincs készleten"}</p>
+    </div>
   )
 }
 
